59707e65cde26d900831b90823c20919f8590764
CorteoJack <85971905+CorteoJack@users.noreply.github.com>
2021-06-15 20:44:31 -0400

    added comment

--- KEV.py
+++ KEV.py
@@ -1,9 +1,11 @@
 import tkinter as tk
-from tkinter.filedialog import*
+from tkinter.filedialog import *
 
-filepath = askopenfilename(title="Ouvrir une image", filetypes =[('png files','.png'),('all files','.*')])
-fenetre =Tk()
+filepath = askopenfilename(
+    title="Ouvrir une image", filetypes=[("png files", ".png"), ("all files", ".*")]
+)
+fenetre = Tk()
 photo = PhotoImage(file=filepath)
-canvas = Canvas(fenetre, width =photo.width(), height=photo.height(), bg="yellow")
-canvas.create_image(50,50,anchor=NW, image=photo)
+canvas = Canvas(fenetre, width=photo.width(), height=photo.height(), bg="yellow")
+canvas.create_image(50, 50, anchor=NW, image=photo)
 canvas.pack()
